e023683b61ecaa0f9555f2036f88e3c75885492e
EloYoshida <helolondrina@gmail.com>
2020-03-15 20:57:32 -0300

    criando func atualizar filme refaturado metodo buscar filme , foi retirado do filme p classe DAO foi add jar MySQL ao projeto

--- src/Teste.java
+++ src/Teste.java
@@ -1,6 +1,10 @@
-import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import ads.pipoca.controller.ManterFilmes;
+import ads.pipoca.model.entity.Filme;
+import ads.pipoca.model.entity.Genero;
 
 public class Teste {
 
@@ -9,9 +13,36 @@ public class Teste {
 		try {
 			manter.listarGeneros();
 			int id = manter.inserirFilme();
-			manter.listarFilme(id);
-			System.out.println("=======================");
-		} catch (IOException e) {
+			Filme filme = manter.listarFilme(id);
+			
+			//atualizar
+			filme.setTitulo("O Naufrago2");
+			filme.setDescricao("Chuck Noland  (Tom Hanks) um inspetor da Federal Express (FedEx), "
+					+ "multinacional encarregada de enviar cargas e correspondências, que tem por "
+					+ "função checar vários escritórios da empresa pelo planeta. Porém, em uma de "
+					+ "suas ....");
+			filme.setDiretor("Eloiza Yoshida");
+
+			Genero genero = new Genero();
+			genero.setId(16);
+			filme.setGenero(genero);
+
+			DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+			try {
+				filme.setDataLancamento(formatter.parse("15/03/2020"));
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				filme.setDataLancamento(null);
+			}
+
+			filme.setPopularidade(50.25);
+			filme.setPosterPath("img/naufrago2.jpg");
+			manter.atualizarFilme(filme);
+			System.out.println("=============   APOS ATUALIZACAO  =================================");
+			System.out.println(filme);
+			
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
